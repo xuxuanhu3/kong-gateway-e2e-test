0749db4fa939514a1d4bce548717abaf384d7053
Yali <35449232+yyashYin@users.noreply.github.com>
2025-03-12 21:14:23 +0800

    update route path

--- tests/test-constants.ts
+++ tests/test-constants.ts
@@ -1,5 +1,5 @@
 export const testServiceName = 'example_service';
 export const testRouteName = 'example_route';
-export const testRoutePath = '/example';
+export const testRoutePath = '/route';
 export const upstreamURL = 'https://httpbin.konghq.com';
 export const gatewayServiceUrl = "http://localhost:8001/services";
